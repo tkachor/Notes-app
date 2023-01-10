a944bcf2c0ad2597d56a0787f1e789b388b61b9c
TKachor <tkachor@gmail.com>
2023-01-10 16:32:08 +0200

    Add AssemblyMappingProfile class

---
+++ Notes.Backend/Notes.Aplication/Common/Mappings/AssemblyMappingProfile.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Aplication.Common.Mappings
+{
+    internal class AssemblyMappingProfile : Profile
+    {
+        public AssemblyMappingProfile(Assembly assembly) => ApplyMappingForAssembly(assembly);
+
+        private void ApplyMappingForAssembly(Assembly assembly)
+        {
+            var types = assembly.GetExportedTypes()
+                .Where(type => type.GetInterfaces()
+                .Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IMapWith<>)))
+                .ToList();
+
+            foreach (var type in types)
+            {
+                var instance = Activator.CreateInstance(type);
+                var methodInfo = type.GetMethod("Mapping");
+                methodInfo?.Invoke(instance, new object[] {this});
+
+            }
+        }
+    }
+}
---
+++ Notes.Backend/Notes.Aplication/Common/Mappings/IMapWith.cs
@@ -0,0 +1,9 @@
+﻿using AutoMapper;
+
+namespace Notes.Aplication.Common.Mappings
+{
+    internal interface IMapWith<T>
+    {
+        void Mapping(Profile profile) => profile.CreateMap(typeof(T), GetType());
+    }
+}
