ea99c8af0934f6dafe1192dffb231801538dd58e
TKachor <tkachor@gmail.com>
2023-01-10 22:16:02 +0200

    Add UpdateNote comands

---
+++ Notes.Backend/Notes.Aplication/Notes/Commands/UpdateNote/UpdateNoteCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Notes.Aplication.Notes.Commands.UpdateNote
+{
+    internal class UpdateNoteCommand : IRequest
+    {
+        public Guid UserId { get; set; }
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+    }
+}
---
+++ Notes.Backend/Notes.Aplication/Notes/Commands/UpdateNote/UpdateNoteCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Notes.Aplication.Interfaces;
+using Notes.Domain;
+
+namespace Notes.Aplication.Notes.Commands.UpdateNote
+{
+    internal class UpdateNoteCommandHandler : IRequestHandler<UpdateNoteCommand>
+    {
+        private readonly INotesDbContext _dbContext;
+
+        public UpdateNoteCommandHandler(INotesDbContext dbContext) =>
+            _dbContext = dbContext;
+
+        public async Task<Unit> Handle(UpdateNoteCommand request,
+            CancellationToken cancellationToken)
+        {
+            var entity =
+                await _dbContext.Notes.FirstOrDefaultAsync(note =>
+                    note.Id == request.Id, cancellationToken);
+
+            if (entity == null || entity.UserId != request.UserId)
+            {
+                throw new NotFoundException(nameof(Note), request.Id);
+            }
+
+            entity.Details = request.Details;
+            entity.Title = request.Title;
+            entity.EditDate = DateTime.Now;
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+}
