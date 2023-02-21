9920a9e09e3c0e2611f0cc9858690686709d7363
TKachor <tkachor@gmail.com>
2023-02-21 19:57:16 +0200

    feature: add queries getnote snd getnotelist

---
+++ Notes.Backend/Notes.Aplication/Notes/Queries/GetNoteList/NoteLookupDto.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Notes.Aplication.Common.Mappings;
+using Notes.Domain;
+
+namespace Notes.Aplication.Notes.Queries.GetNoteList
+{
+    public class NoteLookupDto : IMapWith<Note>
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Note, NoteLookupDto>()
+                .ForMember(noteDto => noteDto.Id,
+                    opt => opt.MapFrom(note => note.Id))
+                .ForMember(noteDto => noteDto.Title,
+                    opt => opt.MapFrom(note => note.Title));
+        }
+    }
+}
